dfb30e9779778517b0a01bea46a1fcf06af5e10e
dmalani256 <dmalani256@gmail.com>
2024-06-14 15:10:37 -0400

    add degree image to education tab

--- app/Education.js
+++ app/Education.js
@@ -1,4 +1,5 @@
 import style from "./home.module.css";
+import degree from './degree.jpg';
 const Education = () => (
   <div className={style.div}>
     <h2 className={style.h2}>Education</h2>
@@ -8,6 +9,8 @@ const Education = () => (
     <p className={style.p}>Duration: August 2019 - May 2023</p>
     <p className={style.p}>GPA: 3.5</p>
     <p className={style.p}>Status: Completed</p>
+    <Image src={degree} alt="Degree image" width={210} height={250} />
   </div>
+
 );
 export default Education;
